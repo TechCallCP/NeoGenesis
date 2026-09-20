bb74c00a11acad3a627e0ce5ae868d7845c8c851
captain <chandler.price3568@gmail.com>
2026-09-19 23:51:03 -0400

    fixed mekanism error for compat

--- src/main/java/shipwrights/genesis/mixin/compat/mekanism/AdvancedSolarGeneratorMixin.java
+++ src/main/java/shipwrights/genesis/mixin/compat/mekanism/AdvancedSolarGeneratorMixin.java
@@ -4,19 +4,17 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import shipwrights.genesis.NeoGenesisMod;
 
+import java.lang.reflect.Method;
+
 @Pseudo
 @Mixin(targets = "mekanism.generators.common.tile.TileEntityAdvancedSolarGenerator", remap = false)
 public abstract class AdvancedSolarGeneratorMixin {
-
-    @Shadow
-    protected abstract mekanism.api.math.FloatingLong getConfiguredMax();
 
     @Inject(method = "checkCanSeeSun", at = @At("HEAD"), cancellable = true)
     private void genesis$checkCanSeeSun(CallbackInfoReturnable<Boolean> cir) {
@@ -30,7 +28,19 @@ public abstract class AdvancedSolarGeneratorMixin {
     private void genesis$getProduction(CallbackInfoReturnable<Object> cir) {
         Level level = ((BlockEntity) (Object) this).getLevel();
         if (level != null && NeoGenesisMod.isSpaceDimension(level)) {
-            cir.setReturnValue(getConfiguredMax());
+            Object max = genesis$getConfiguredMax();
+            if (max != null) {
+                cir.setReturnValue(max);
+            }
+        }
+    }
+
+    private Object genesis$getConfiguredMax() {
+        try {
+            Method method = this.getClass().getMethod("getConfiguredMax");
+            return method.invoke(this);
+        } catch (Exception e) {
+            return null;
         }
     }
 }
